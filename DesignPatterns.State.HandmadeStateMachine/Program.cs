c01f84023a8f6540caeb846bdd0757d8ff433825
fatihmert-medyanet <fatih.mert@medyanet.com.tr>
2017-09-28 10:42:54 +0300

    Interpreter and State inserted

---
+++ DesignPatterns.State.HandmadeStateMachine/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stateless;
+
+namespace DesignPatterns.State.HandmadeStateMachine
+{
+    public enum Health
+    {
+        NonReproductive,
+        Pregnant,
+        Reproductive
+    }
+
+    public enum Activity
+    {
+        GiveBirth,
+        ReachPuberty,
+        HaveAbortion,
+        HaveUnprotectedSex,
+        Historectomy
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stateMachine = new StateMachine<Health, Activity>(Health.NonReproductive);
+            stateMachine.Configure(Health.NonReproductive)
+                .Permit(Activity.ReachPuberty, Health.Reproductive);
+        }
+    }
+}
